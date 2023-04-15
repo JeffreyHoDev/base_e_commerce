ed17f89ba8b3e5d05f50035df6725c24ac75c452
JeffreyHo <jeffreyhodev@gmail.com>
2023-04-15 23:12:38 +0800

    implement some mobile styling, change favico, implement service worker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 
 import reportWebVitals from './reportWebVitals';
-
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import {
   RouterProvider,
 } from "react-router-dom";
@@ -22,6 +22,11 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
